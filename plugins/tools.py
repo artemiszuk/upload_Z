93be5fd44db3f60ef390e8f7dc805e1bfa4ad8c5
Sourcery AI <>
2022-01-20 14:23:18 +0000

    'Refactored by Sourcery'

--- plugins/tools.py
+++ plugins/tools.py
@@ -16,7 +16,7 @@ from plugins.progress import humanbytes
 
 
 async def speedtst(client, message):
-    message = await message.reply_text(f"Performing Speedtest ...")
+    message = await message.reply_text('Performing Speedtest ...')
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
@@ -27,7 +27,7 @@ async def speedtst(client, message):
         test.results.share()
         result = test.results.dict()
     except Exception as e:
-        await message.edit_text(f"{str(e)}")
+        await message.edit_text(f'{e}')
     path = wget.download((result["share"]))
     await message.reply_photo(photo=path)
     await message.delete()
@@ -41,7 +41,7 @@ async def dl_link(app, message):
                         "Queue Added")
       return "",bot_msg"""
     url = message.text.split()[-1]  # seperate link from message
-    text = f"Checking url..."
+    text = 'Checking url...'
     bot_msg = await app.send_message(
         user_id, text, disable_web_page_preview=True
     )  # displays user input
@@ -54,7 +54,7 @@ async def dl_link(app, message):
             downloader.start(blocking=False)
             file_name = os.path.basename(url)
             while not downloader.isFinished():
-                total_length = downloader.filesize if downloader.filesize else 0
+                total_length = downloader.filesize or 0
                 downloaded = downloader.get_dl_size()
                 percentage = downloader.get_progress() * 100
                 prg = progress(int(percentage), 100)
@@ -85,9 +85,8 @@ async def dl_link(app, message):
             filepath = f"{path}/{f[0]}"
             return filepath, bot_msg
         except Exception as e:
-            e_text = str(e)
             shutil.rmtree(f"download/{user_id}/")
-            print(e_text)
+            print(e)
             await bot_msg.edit_text("Some Error Occcured,Can't Download ")
             return
 
@@ -105,27 +104,24 @@ def is_url(url):
 
 
 def progress(current, total):
-    bar = ""
     current = int(current / 10)
-    for i in range(0, 10):
-        if i < current:
-            bar += "█"
-        else:
-            bar += "░"
+    bar = "".join("█" if i < current else "░" for i in range(10))
     return f"[{bar}]"
 
 
 async def get_details(filepath):
-    mydict = dict()
     probe = ffmpeg.probe(filepath)
     video_stream = next(
         (stream for stream in probe["streams"] if stream["codec_type"] == "video"), None
     )
-    mydict["width"] = int(video_stream["width"])
-    mydict["height"] = int(video_stream["height"])
-    mydict["duration"] = probe["format"]["duration"]
+    mydict = {
+        'width': int(video_stream["width"]),
+        'height': int(video_stream["height"]),
+        'duration': probe["format"]["duration"],
+    }
+
     filename = os.path.basename(filepath)
-    mydict["tname"] = filename[0 : filename.index(".")] + ".jpeg"
+    mydict["tname"] = filename[:filename.index(".")] + ".jpeg"
     (
         ffmpeg.input(filepath, ss=random.randrange(0, int(float(mydict["duration"]))))
         .filter("scale", mydict["width"], -1)
